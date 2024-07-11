df58dfee5d2668d0a399b6f48a5fbbb26a266419
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2024-07-11 11:25:12 +0300

    add animation for hero

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -24,7 +24,17 @@ export default function Home({ setSelectedPage }: Props) {
         onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
       >
         <div className="z-10 mt-32 md:basis-3/5">
-          <motion.div className="md:-mt-20">
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.75 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="" />
@@ -37,12 +47,23 @@ export default function Home({ setSelectedPage }: Props) {
             </p>
           </motion.div>
 
-          <motion.div className="mt-8 flex items-center gap-8">
+          <motion.div
+            className="mt-8 flex items-center gap-8"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.3, duration: 0.75 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <PrimaryBtn setSelectedPage={setSelectedPage}>Join now</PrimaryBtn>
-            <AnchorLink 
-                className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-                onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-                href={`#${SelectedPage.ContactUs}`}>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+            >
               <p>Learn more</p>
             </AnchorLink>
           </motion.div>
@@ -55,9 +76,9 @@ export default function Home({ setSelectedPage }: Props) {
         <div className="h-[150px] w-full bg-primary-100 py-10">
           <div className="mx-auto w-5/6">
             <div className="flex w-3/5 items-center justify-between gap-8">
-              <img src={SponserRedBull} alt="" />
-              <img src={SponserForbes} alt="" />
-              <img src={SponserFortune} alt="" />
+              <img src={SponserRedBull} alt="sponser-redbull" />
+              <img src={SponserForbes} alt="sponser-forbes" />
+              <img src={SponserFortune} alt="sponser-fortune" />
             </div>
           </div>
         </div>
