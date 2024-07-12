c10e245e62f20268d0cc44b67fd11b6978be2a62
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2024-07-12 16:18:42 +0300

    add image and description  for benefits section

--- src/sections/Benifits.tsx
+++ src/sections/Benifits.tsx
@@ -1,5 +1,6 @@
 import { BenefitType, SelectedPage } from "@/assets/data";
 import BenefitCard from "@/components/BenefitCard";
+import PrimaryBtn from "@/components/PrimaryBtn";
 import {
   HomeModernIcon,
   UserGroupIcon,
@@ -7,6 +8,7 @@ import {
 } from "@heroicons/react/24/solid";
 
 import { motion } from "framer-motion";
+import BenefitsImg from "@/assets/images/BenefitsPageGraphic.png"
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -31,11 +33,11 @@ const benefits: Array<BenefitType> = [
 ];
 
 const container = {
-    hidden: {},
-    visible: {
-      transition: { staggerChildren: 0.2 },
-    },
-  };
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
 
 export default function Benifits({ setSelectedPage }: Props) {
   return (
@@ -43,17 +45,17 @@ export default function Benifits({ setSelectedPage }: Props) {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <motion.div 
-            className="md:my-5 md:w-3/5"
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.5 }}
-            transition={{ duration: 0.75 }}
-            variants={{
-              hidden: { opacity: 0, x: -50 },
-              visible: { opacity: 1, x: 0 },
-            }}
-            >
+        <motion.div
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.75 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <h1 className="header-text uppercase">More than just a gym.</h1>
           <p className="text-sm my-5">
             We provide world class fitness equipment, trainers and classes to
@@ -61,16 +63,15 @@ export default function Benifits({ setSelectedPage }: Props) {
             care into each and every member.
           </p>
         </motion.div>
-        <motion.div 
-            className="mt-5 items-center justify-between gap-8 md:flex"
-            initial="hidden"
+        <motion.div
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
           variants={container}
-          >
+        >
           {benefits.map((benefit: BenefitType) => (
             <BenefitCard
-              
               icon={benefit.icon}
               title={benefit.title}
               desc={benefit.desc}
@@ -78,6 +79,71 @@ export default function Benifits({ setSelectedPage }: Props) {
             />
           ))}
         </motion.div>
+
+        {/* Graphic and Description */}
+        <div className="mt-16 md:flex  md:mt-28 items-center justify-between gap-20 ">
+          {/* image */}
+          <img 
+              className="mx-auto"
+              src={BenefitsImg} alt="Benefits image" />
+
+          <div>
+            {/* title */}
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.75 }}
+                    variants={{
+                      hidden: { opacity: 0, x: 50 },
+                      visible: { opacity: 1, x: 0 },
+                    }}>
+                  <h1 className="header-text uppercase">
+                    Millions of happy members getting{" "}
+                    <span className="text-primary-500">fit.</span>
+                  </h1>
+                </motion.div>
+              </div>
+            </div>
+
+            {/* description */}
+            <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ delay: 0.3 , duration: 0.75 }}
+                variants={{
+                  hidden: { opacity: 0, x: 50 },
+                  visible: { opacity: 1, x: 0 },
+                }}>
+              <p className="my-5">
+                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                Felis orci diam odio.
+              </p>
+              <p className="mb-5">
+                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                enim mattis odio in risus nunc.
+              </p>
+            </motion.div>
+
+            {/* button */}
+            <div className="relative mt-16">
+                <div className="before:absolute before:-bottom-20 before:right-40 before:z-[-1] before:content-sparkles">
+                    <div className="relative">
+                        <PrimaryBtn setSelectedPage={setSelectedPage}>Join now</PrimaryBtn>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
